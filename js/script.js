cfea0e525cdb688b9eac69fb79e7ed020eb2e3b8
SteveNoyes <stevenmnoyes.af@gmail.com>
2022-12-02 20:52:11 -0500

    there it is

--- js/script.js
+++ js/script.js
@@ -1,38 +1,26 @@
-
-
-
-
-
 function ValidateEmail(inputText) {
-  // initialize
+  // initialize variables
   let field = document.getElementById("femail");
   let error = document.getElementById("errorDiv");
-
   // regex pattern
   let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-  // if inputText matches the regex pattern
-  // if(inputText.value.match(mailformat)) {
-  //   field.classList.remove("err");
-  //   error.innerHTML = "Whoops! It looks like you forgot to add your email";
-  // }
-
-  // if inputText is empty
-  if(inputText.value == '') {
+  if(inputText.value.match(mailformat)) {
+    // remove error class
+    field.classList.remove("err");
+    // temorary message
+    error.innerHTML = "You did it";
+  // else if inputText is empty
+  } else if(inputText.value == '') {
     // add error class
     field.classList.add("err");
     // add error message
     error.innerHTML = "Whoops! It looks like you forgot to add your email";
-
-  } else if(inputText.value !== mailformat) {
-    field.classList.remove("err");
+  // else if inputText does not match regex 
+  }else if(inputText.value != mailformat) {
+    // add error class
+    field.classList.add("err");
+    // add error message
     error.innerHTML = "Please provide a valid email address";
-  } else {
-    error.innerHTML = "You did it";
-  }
+  } 
 }
-
-
-
-
-
